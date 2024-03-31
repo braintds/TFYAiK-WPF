923516ff5eb593493fe8436fa742db0b65ecbc64
Kirill <freestyle.ff50@gmail.com>
2024-04-01 00:44:59 +0700

    Add Grid and GridSplitter

--- TFYAiK WPF/MainWindow.xaml.cs
+++ TFYAiK WPF/MainWindow.xaml.cs
@@ -24,5 +24,165 @@ namespace TFYAiK_WPF
         {
             InitializeComponent();
         }
+
+        private void StartMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CreateMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveAsMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExitMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UndoMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RedoMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CutMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CopyMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PasteMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TaskMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GrammarMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GrammarClasifyMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MethodAnalisMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DiagnosticsMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TestExampleMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BibliographyMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SourceCodeMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ZoomPlusMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ZoomMinusMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ReferenceMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AboutMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void СreateNewButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UndoButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RedoButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PasteButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CopyButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CutButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
