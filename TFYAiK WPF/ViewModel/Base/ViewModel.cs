bc3d22f05e2c601e9de08bbae44d670dc4dfcdef
Kirill <freestyle.ff50@gmail.com>
2024-04-01 23:01:24 +0700

    try to add splitter and mvvm

---
+++ TFYAiK WPF/ViewModel/Base/ViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFYAiK_WPF.ViewModel.Base
+{
+    abstract class ViewModel : INotifyPropertyChanged, IDisposable
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if(Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        private bool _Disposed;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposing || _Disposed) return;
+            _Disposed = true;
+            // освобождение управляемых ресурсов
+        }
+    }
+}
---
+++ TFYAiK WPF/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TFYAiK_WPF.ViewModel.Base;
+
+namespace TFYAiK_WPF.ViewModel
+{
+    class MainWindowViewModel : ViewModel.Base.ViewModel
+    {
+        #region Заголовок окна
+        private string _Title = "PASCAL: логические выражения";
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+#endregion
+    }
+}
