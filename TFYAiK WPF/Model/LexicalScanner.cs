742d3892e35fbb5f7979a0abd24b64467cb1ee11
Kirill <freestyle.ff50@gmail.com>
2024-04-01 23:10:14 +0700

    Added basic logic of lexical analyzer

---
+++ TFYAiK WPF/Model/LexicalScanner.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFYAiK_WPF.Model
+{
+    public static class LexicalScanner
+    {
+        public struct LexicalItem
+        {
+            public Codes lexicalCode;
+            public string item;
+            public int startPosition;
+            public int endPosition;
+
+            public LexicalItem(Codes code, string item, int startPosition, int endPosition)
+            {
+                this.lexicalCode = code;
+                this.item = item;
+                this.startPosition = startPosition;
+                this.endPosition = endPosition;
+            }
+
+            public override string ToString()
+            {
+                return $"{startPosition}:{endPosition}  {item}: {lexicalCode}: {Convert.ToInt16(lexicalCode)}";
+            }
+        }
+
+        public enum Codes
+        {
+            ErrorCode = -1,
+            IdentifierCode = 1,         // {a-z, A_Z, _}*
+            IntegerConstCode,           // {0-9}*
+            DoubleConstCode,            // {0-9}*{.} & {0-9}*
+            RelationalOpCode,               // .EQ. (=) | .NE. (!=) | .GT. (>)
+            AdditiveOpCode, MultiplicateOpCode, // + - * /
+            LogicalOpCode, LogicalConstantCode,
+            NotOpCode,
+            LeftParenCode, RightParenCode,
+        }
+
+        public static bool IsLogicalOp(Codes code)
+        {
+            switch (code)
+            {
+                case Codes.LogicalOpCode:
+                    //case Codes.LogicalAndCode:
+                    //case Codes.LogicalOrCode:
+                    return true;
+                default:
+                    break;
+            }
+            return false;
+        }
+
+        private static Codes IsArithmOperator(string text)
+        {
+            if (text.Length > 1)
+            {
+                return Codes.ErrorCode;
+            }
+
+            switch (text)
+            {
+                case "+":
+                case "-":
+                    return Codes.AdditiveOpCode;
+                case "*":
+                case "/":
+                    return Codes.MultiplicateOpCode;
+                default:
+                    return Codes.ErrorCode;
+            }
+        }
+
+        private static Codes IsOperator(string text)
+        {
+            if (text == "")
+            {
+                return Codes.ErrorCode;
+            }
+
+            switch (text.ToUpperInvariant())
+            {
+                case ".EQ.":
+                case ".NE.":
+                case ".GT.":
+                case ".GE.":
+                case ".LT.":
+                case ".LE.":
+                    return Codes.RelationalOpCode;
+                case ".NOT.":
+                    return Codes.NotOpCode;
+                case ".AND.":
+                case ".OR.":
+                    return Codes.LogicalOpCode;
+                default: return Codes.ErrorCode;
+            }
+        }
+
+        private static Codes IsNumber(string text)
+        {
+            if (text == "")
+            {
+                return Codes.ErrorCode;
+            }
+
+            int integerConst = 0;
+
+            if (int.TryParse(text, out integerConst))
+            {
+                return Codes.IntegerConstCode;
+            }
+            // Проверяет первое число на то что это цифра. \ Checks if the first number is a digit
+            if (Char.IsDigit(text[0]))
+            {
+                int i = 0;
+                int countDot = 0;
+
+                while (i < text.Length)
+                {
+                    if (text[i] == '.')
+                    {
+                        countDot++;         // Точка должна быть только одна \ There must be only one point
+                    }
+                    i++;
+                }
+
+                if (countDot == 1)
+                {
+                    return Codes.DoubleConstCode;
+                }
+            }
+            // Если TryParse вернул false или в строке больше одной точки.
+            // \ If TryParse returned false or there is more than one dot in the string.
+            return Codes.ErrorCode;
+        }
+
+        private static Codes IsIdentifier(string text)
+        {
+            if (text == "")
+            {
+                return Codes.ErrorCode;
+            }
+
+            if (!Char.IsLetter(text[0]))
+            {
+                return Codes.ErrorCode;
+            }
+            else
+            {
+                foreach (char c in text)
+                {
+                    if (c != '_' && !Char.IsDigit(c) && !Char.IsLetter(c))
+                    {
+                        return Codes.ErrorCode;
+                    }
+                }
+            }
+            return Codes.IdentifierCode;
+        }
+
+        private static Codes IsBracket(string text)
+        {
+            if (text == "")
+            {
+                return Codes.ErrorCode;
+            }
+
+            switch (text)
+            {
+                case "(": return Codes.LeftParenCode;
+                case ")": return Codes.RightParenCode;
+                default: return Codes.ErrorCode;
+            }
+        }
+
+        private static Codes IsLogicalConstant(string text)
+        {
+            switch (text.ToUpper())
+            {
+                case "TRUE":
+                case "FALSE":
+                    return Codes.LogicalConstantCode;
+                default:
+                    return Codes.ErrorCode;
+            }
+        }
+
+        private static char GetNext(string text, int currentPosition)
+        {
+            return text[currentPosition + 1];
+        }
+
+        public static List<LexicalItem> GetTokens(string inputString)
+        {
+            int i = 0;
+            string answer = "";
+            var parts = new List<LexicalItem>();
+            string subString = "";
+
+            while (i < inputString.Length)
+            {
+                char c = inputString[i];
+
+                // Это скобки. \ It's brackets.
+                if (c == ')' || c == '(')
+                {
+                    i++;
+                    parts.Add(new LexicalItem(IsBracket(c.ToString()), c.ToString(), i, i));
+                    continue;
+                }
+
+                // Может быть оператором. \ Can be an operator.
+                if (c == '.')
+                {
+                    subString = "";
+                    int start = i + 1;
+                    int countDot = 0;
+
+                    while ((i < inputString.Length - 1) && (char.IsLetter(inputString[i]) || inputString[i] == '.'))
+                    {
+                        if (countDot < 2)
+                        {
+                            subString += inputString[i];
+
+                            if (inputString[i] == '.')
+                            {
+                                countDot++;
+                            }
+                        }
+                        else
+                        {
+                            break;
+                        }
+                        i++;
+                    }
+
+                    if (subString.EndsWith("."))
+                    {
+                        //parts.Add($"{start}:{i}", IsOperator(subString));
+                        parts.Add(new LexicalItem(IsOperator(subString), subString, start, i));
+                    }
+
+                    subString = "";
+                }
+
+                // Может быть идентификатором. \ Can be an identifier.
+                if (Char.IsLetter(c))
+                {
+                    subString = "";
+                    int start = i + 1;
+
+                    while ((i < inputString.Length) && (Char.IsLetter(inputString[i]) || Char.IsDigit(inputString[i])))
+                    {
+                        subString += inputString[i];
+                        i++;
+                    }
+                    if (IsLogicalConstant(subString) == Codes.LogicalConstantCode)
+                    {
+                        parts.Add(new LexicalItem(IsLogicalConstant(subString), subString, start, i));
+                        continue;
+                    }
+
+                    parts.Add(new LexicalItem(IsIdentifier(subString), subString, start, i));
+                }
+
+                // Может быть числом. \ Can be a number.
+                if (Char.IsDigit(c))
+                {
+                    subString = "";
+                    int start = i + 1;
+
+                    while ((i < inputString.Length) && (Char.IsDigit(inputString[i]) || inputString[i] == '.'))
+                    {
+                        subString += inputString[i];
+                        i++;
+                    }
+
+                    if ((i < inputString.Length - 1) && Char.IsLetter(GetNext(inputString, i)))
+                    {
+                        subString = subString.Remove(subString.LastIndexOf('.'), 1);
+                        i--;
+                    }
+
+                    if (subString.EndsWith("."))
+                    {
+                        //parts.Add($"{start}:{i}", Codes.ErrorCode);
+                        parts.Add(new LexicalItem(Codes.ErrorCode, subString, start, i));
+                    }
+                    else
+                    {
+                        //parts.Add($"{start}:{i}", IsNumber(subString));
+                        parts.Add(new LexicalItem(IsNumber(subString), subString, start, i));
+                    }
+
+                    subString = "";
+                }
+
+                // Это арифметический оператор. \ It is an arithmetic operator.
+                if (c == '+' || c == '-' || c == '*' || c == '/')
+                {
+                    i++;
+                    //parts.Add(i.ToString(), IsArithmOperator(c.ToString()));
+                    parts.Add(new LexicalItem(IsArithmOperator(c.ToString()), c.ToString(), i, i));
+                    continue;
+                }
+            }
+
+            return parts;
+        }
+    }
+}
---
+++ TFYAiK WPF/Model/Parser.cs
@@ -0,0 +1,346 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static TFYAiK_WPF.Model.LexicalScanner;
+
+namespace TFYAiK_WPF.Model
+{
+    class Parser
+    {
+        public struct ParsingError
+        {
+            public enum ActionOverItem
+            {
+                Remove,
+                Replace,
+                InsertAfter,
+                InsertBefore,
+            }
+            public int position { get; set; }
+            public LexicalScanner.Codes expectedItem { get; set; }
+            public ActionOverItem action { get; set; }
+            public string message { get; set; }
+
+            public ParsingError(LexicalScanner.Codes expectedItem, ActionOverItem action, int position, string message)
+            {
+                this.expectedItem = expectedItem;
+                this.action = action;
+                this.position = position;
+                this.message = message;
+            }
+        }
+        private static List<LexicalItem> s_tokens;
+        private static int s_currentTokenIndex;
+        private static bool s_IsBoolIdentidier;
+        public static List<ParsingError> s_errors { get; private set; }
+
+        public static void ClearErrorsList() { s_errors.Clear(); }
+
+        // Проверка текущего токена заданному
+        private static bool Check(LexicalScanner.Codes expectedCode)
+        {
+            if (s_currentTokenIndex == s_tokens.Count)
+            {
+                return false;
+            }
+            return s_tokens[s_currentTokenIndex].lexicalCode == expectedCode;
+        }
+
+        // В случае соответсвия токена переход к следующему, иначе сообщение об ошибке
+        private static void Match(LexicalScanner.Codes expectedCode)
+        {
+            if (s_currentTokenIndex < s_tokens.Count)
+            {
+                if (s_tokens[s_currentTokenIndex].lexicalCode == expectedCode)
+                {
+                    s_currentTokenIndex++;
+                }
+                else
+                {
+                    if (!Check(LexicalScanner.Codes.ErrorCode) && !Check(LexicalScanner.Codes.RightParenCode))
+                    {
+                        var error = new ParsingError(expectedCode, ParsingError.ActionOverItem.InsertBefore, s_tokens[s_currentTokenIndex].startPosition - 1, $"Ожидалось: {expectedCode}, получен {s_tokens[s_currentTokenIndex].lexicalCode}");
+                        s_errors.Add(error);
+                    }
+                    else
+                    {
+                        var error = new ParsingError(expectedCode, ParsingError.ActionOverItem.InsertBefore, s_tokens[s_currentTokenIndex].startPosition, $"Ожидалось: {expectedCode}, получен {s_tokens[s_currentTokenIndex].lexicalCode}");
+                        s_errors.Add(error);
+                    }
+                    throw new Exception($"Ожидалась лексема {expectedCode}, получена {s_tokens[s_currentTokenIndex].lexicalCode}");
+                }
+            }
+        }
+
+        // Проверка на идентификатор \ левую скобку \ целое и дробное число
+        private static bool IsLiteral(int i)
+        {
+            return (s_tokens[s_currentTokenIndex + i].lexicalCode == Codes.IdentifierCode
+                || s_tokens[s_currentTokenIndex + i].lexicalCode == Codes.LeftParenCode
+                || s_tokens[s_currentTokenIndex + i].lexicalCode == Codes.DoubleConstCode
+                || s_tokens[s_currentTokenIndex + i].lexicalCode == Codes.IntegerConstCode);
+        }
+
+        // Инициализация парсинга
+        public static void ParseInit(List<LexicalItem> inputTokens)
+        {
+            s_tokens = inputTokens;
+            s_currentTokenIndex = 0;
+            s_errors = new List<ParsingError>();
+            Parse();
+        }
+
+        // Парсинг
+        public static void Parse()
+        {
+
+            try
+            {
+                OrExpr();
+                if (s_currentTokenIndex != s_tokens.Count)
+                {
+                    string message = "Некорректный токен";
+                    if (s_currentTokenIndex == s_tokens.Count - 1)
+                    {
+                        var error = new ParsingError(LexicalScanner.Codes.ErrorCode, ParsingError.ActionOverItem.Remove, s_tokens[s_currentTokenIndex].startPosition, message);
+                        s_errors.Add(error);
+                        throw new Exception(message);
+                    }
+                    else
+                    {
+                        if (IsLiteral(0))
+                        {
+                            message = "Ожидался оператор сравнения";
+                            var error = new ParsingError(Codes.RelationalOpCode, ParsingError.ActionOverItem.InsertBefore, s_tokens[s_currentTokenIndex].startPosition - 1, message);
+                            s_errors.Add(error);
+                            throw new Exception(message);
+                        }
+                        else if (IsLiteral(1))
+                        {
+                            message = "Ожидался оператор сравнения";
+                            if (!Check(Codes.ErrorCode) && !Check(Codes.RightParenCode))
+                            {
+                                var error = new ParsingError(Codes.RelationalOpCode, ParsingError.ActionOverItem.InsertBefore, s_tokens[s_currentTokenIndex].startPosition - 1, message);
+                                s_errors.Add(error);
+                            }
+                            else
+                            {
+                                var error = new ParsingError(Codes.RelationalOpCode, ParsingError.ActionOverItem.Replace, s_tokens[s_currentTokenIndex].startPosition, message);
+                                s_errors.Add(error);
+                            }
+                            throw new Exception(message);
+                        }
+                        else
+                        {
+                            var error = new ParsingError(Codes.ErrorCode, ParsingError.ActionOverItem.Remove, s_tokens[s_currentTokenIndex].startPosition, message);
+                            s_errors.Add(error);
+                            throw new Exception(message);
+                        }
+                    }
+                }
+            }
+            catch //Нейтрализация ошибки
+            {
+                var token = new LexicalItem(s_errors[s_errors.Count - 1].expectedItem, " ", s_errors[s_errors.Count - 1].position, s_errors[s_errors.Count - 1].position);
+                if (s_tokens.Count > 1)
+                {
+                    if (s_currentTokenIndex < s_tokens.Count)
+                    {
+                        switch (s_errors[s_errors.Count - 1].action)
+                        {
+                            case ParsingError.ActionOverItem.InsertBefore:
+                                s_tokens.Insert(s_currentTokenIndex, token);
+                                break;
+                            case ParsingError.ActionOverItem.InsertAfter:
+                                s_tokens.Insert(s_currentTokenIndex, token);
+                                break;
+                            case ParsingError.ActionOverItem.Replace:
+                                s_tokens.RemoveAt(s_currentTokenIndex);
+                                s_tokens.Insert(s_currentTokenIndex, token);
+                                break;
+                            case ParsingError.ActionOverItem.Remove:
+                                s_tokens.RemoveAt(s_currentTokenIndex);
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        s_tokens.Add(token);
+                    }
+                    s_currentTokenIndex = 0;
+                    Parse();
+                }
+                else
+                {
+                    s_tokens.Remove(token);
+                    return;
+                }
+            }
+        }
+
+        // <RelExpr> -> <AddExpr>(RealOp <AddExpr>)*
+        private static void OrExpr()
+        {
+            AndExpr();
+            while ((s_currentTokenIndex < s_tokens.Count)
+                && Check(LexicalScanner.Codes.LogicalOpCode)
+                && s_tokens[s_currentTokenIndex].item.ToUpper() == ".OR.")
+            {
+                Match(LexicalScanner.Codes.LogicalOpCode);
+                AndExpr();
+            }
+        }
+
+        // <AndExpr> -> <MulExpr> (AddOp <MulExpr>)*
+        private static void AndExpr()
+        {
+            NotExpr();
+            while ((s_currentTokenIndex < s_tokens.Count)
+                && Check(LexicalScanner.Codes.LogicalOpCode)
+                && s_tokens[s_currentTokenIndex].item.ToUpper() == ".AND.")
+            {
+                Match(LexicalScanner.Codes.LogicalOpCode);
+                NotExpr();
+            }
+        }
+
+        // <NotExpr> -> ! <NotExpr> | <RelExpr>  
+        private static void NotExpr()
+        {
+            if (Check(LexicalScanner.Codes.NotOpCode))
+            {
+                s_IsBoolIdentidier = true;
+                Match(LexicalScanner.Codes.NotOpCode);
+            }
+            RelExpr();
+        }
+
+        // <RelExpr> -> <AddExpr> (RelOp AddExpr)* 
+        public static void RelExpr()
+        {
+            AddExpr();
+            while ((s_currentTokenIndex < s_tokens.Count)
+                && Check(Codes.LogicalOpCode))
+            {
+                Match(Codes.LogicalOpCode);
+                AddExpr();
+            }
+
+        }
+
+        // <AddExpr> -> <MulExpr> (AddOp <MulExpr>)*
+        private static void AddExpr()
+        {
+            MulExpr();
+            while (s_currentTokenIndex < s_tokens.Count
+                && Check(Codes.RelationalOpCode))
+            {
+                Match(LexicalScanner.Codes.RelationalOpCode);
+                MulExpr();
+            }
+
+        }
+
+        // <MulExpr> -> <UnaryExpr>(MulOp <UnaryExpr>)*
+        private static void MulExpr()
+        {
+            UnaryExpr();
+            while ((s_currentTokenIndex < s_tokens.Count)
+                && Check(LexicalScanner.Codes.AdditiveOpCode))
+            {
+                Match(LexicalScanner.Codes.AdditiveOpCode);
+                UnaryExpr();
+            }
+        }
+
+        // <UnaryExpr> -> <Factor> | (AddOp <UnaryExpr>)*
+        private static void UnaryExpr()
+        {
+            Factor();
+            while ((s_currentTokenIndex < s_tokens.Count)
+                && Check(LexicalScanner.Codes.MultiplicateOpCode))
+            {
+                Match(LexicalScanner.Codes.MultiplicateOpCode);
+                Factor();
+            }
+        }
+
+        // <Factor> -> <Identifier> | Const | '(' Expr ')' 
+        private static void Factor()
+        {
+            if (s_currentTokenIndex < s_tokens.Count)
+            {
+                switch (s_tokens[s_currentTokenIndex].lexicalCode)
+                {
+                    case LexicalScanner.Codes.LeftParenCode:
+                        Match(LexicalScanner.Codes.LeftParenCode);
+                        OrExpr();
+                        if (!Check(Codes.RightParenCode))
+                        {
+                            if (s_currentTokenIndex < s_tokens.Count)
+                            {
+                                if (IsLiteral(0))
+                                {
+                                    string message = "Ожидался оператор сравнения";
+                                    var error = new ParsingError(Codes.RelationalOpCode, ParsingError.ActionOverItem.InsertBefore, s_tokens[s_currentTokenIndex].startPosition - 1, message);
+                                    s_errors.Add(error);
+                                    throw new Exception(message);
+                                }
+                                else if (!Check(Codes.ErrorCode) && !Check(Codes.NotOpCode))
+                                {
+                                    string message = "Ожидалась закрывающая скобка";
+                                    var error = new ParsingError(Codes.RightParenCode, ParsingError.ActionOverItem.InsertAfter, s_tokens[s_currentTokenIndex].endPosition + 1, message);
+                                    s_errors.Add(error);
+                                    throw new Exception(message);
+                                }
+                            }
+                            else
+                            {
+                                string message = "Ожидалась закрывающая скобка";
+                                var error = new ParsingError(Codes.RightParenCode, ParsingError.ActionOverItem.InsertAfter, s_tokens[s_tokens.Count - 1].endPosition + 1, message);
+                                s_errors.Add(error);
+                                throw new Exception(message);
+                            }
+                        }
+                        else
+                        {
+                            Match(LexicalScanner.Codes.RightParenCode);
+                        }
+                        break;
+                    case LexicalScanner.Codes.IntegerConstCode:
+                        Match(LexicalScanner.Codes.IntegerConstCode);
+                        break;
+                    case LexicalScanner.Codes.DoubleConstCode:
+                        Match(LexicalScanner.Codes.DoubleConstCode);
+                        break;
+                    case LexicalScanner.Codes.IdentifierCode:
+                        Match(LexicalScanner.Codes.IdentifierCode);
+                        break;
+                    case LexicalScanner.Codes.LogicalConstantCode:
+                        Match(Codes.LogicalConstantCode);
+                        break;
+                    default:
+                        Match(LexicalScanner.Codes.IdentifierCode);
+                        break;
+                }
+            }
+            else
+            {
+                string message = "Некорректный токен";
+                if (s_tokens.Count > 1)
+                {
+                    var error = new ParsingError(Codes.IdentifierCode, ParsingError.ActionOverItem.InsertAfter, s_tokens[s_currentTokenIndex - 1].endPosition + 1, message);
+                    s_errors.Add(error);
+                }
+                else
+                {
+                    var error = new ParsingError(Codes.ErrorCode, ParsingError.ActionOverItem.Remove, s_tokens[s_currentTokenIndex - 1].endPosition, message);
+                    s_errors.Add(error);
+                    throw new Exception(message);
+                }
+            }
+        }
+    }
+}
